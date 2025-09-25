427e66a6519ea1d6bc5a7d214d2974f16c1ab98b
KetDayane <rm95090@estudante.fieb.edu.br>
2025-09-25 11:49:18 -0300

    consumo de rotas

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,14 +1,24 @@
+// Ferramenta de consumo de rotas ou endpoints do backend
 import axios from "axios"
-
+// Hooks do react para controlar estados e renderização
+import {useState, useEffect} from "react"
 const Produtos = () => {
+    // estado com lista de pizzas
+    const [pizzas, setPizzas] = useState([])
     // Objeto com a lita de pizzas
-    axios.get("http://viacep.com.br/ws/01010000/json")
-    .then(response=>{
-            console.log(response.data)
-    })    
-
-    // Iteração da lista de pizzas
-    const listaPizas = pizzas.map(pizza=><li>{pizza}</li>)
+    useEffect(()=>{
+        // GET- busca ps dados da rota ou endpoint 
+        // then - método assincrono, espera o retorno da requisição 
+        // catch - tratamento de erros da requisição
+        axios.get("http://viacep.com.br/ws/01010000/json")
+        .then(response=>{
+            console.log(response.data.data)
+            setPizzas(response.data.data)
+        })
+        .catch(errors=>console.log(errors))
+    }, [])
+        // Iteração da lista de pizzas
+        const listaPizas = pizzas.map(pizza=><li>{pizza}</li>)
     return (
         <div>
             <ul>
